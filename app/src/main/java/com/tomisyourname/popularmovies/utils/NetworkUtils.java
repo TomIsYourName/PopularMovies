af3ce1a0cd2f1b33215aea6a3a7ca651ac53407b
tomisyourname <tomisyourname@gmail.com>
2017-03-03 23:01:01 +0800

    modified gitignore file, and NetworkUtils

--- app/src/main/java/com/tomisyourname/popularmovies/utils/NetworkUtils.java
+++ app/src/main/java/com/tomisyourname/popularmovies/utils/NetworkUtils.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
+import java.io.IOException;
+
 /**
  * Created by zain on 21/02/2017.
  */
@@ -15,5 +17,19 @@ public class NetworkUtils {
         = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
     NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
     return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+  }
+
+  public static boolean isOnline() {
+    Runtime runtime = Runtime.getRuntime();
+    try {
+      Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
+      int exitValue = ipProcess.waitFor();
+      return (exitValue == 0);
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    return false;
   }
 }
